a5093363c35c748c03f1a98399381e135ea252c4
maheshtvazure <83171825+maheshtvazure@users.noreply.github.com>
2021-07-07 20:44:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}, how are you today?</div>;
+  return <div>Hello {value}, how are you today? Hows the weather?</div>;
 }
 
 export default App;
